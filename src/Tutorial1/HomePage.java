48fe728e4bcb0ba8d0159012168b58ffe692cf4f
Manisha Swapnil Rane <manishalonari@gmail.com>
2022-12-19 22:57:33 +0530

    updated

--- src/Tutorial1/HomePage.java
+++ src/Tutorial1/HomePage.java
@@ -4,6 +4,7 @@ public class HomePage {
 
 	public static void main(String[] args) {
 		System.out.println("Home page created");
+		System.out.println("updated home page");
 	}
 
 }
--- src/Tutorial1/LoginPage.java
+++ src/Tutorial1/LoginPage.java
@@ -3,7 +3,7 @@ package Tutorial1;
 public class LoginPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Login Page created");
 
 	}
 
